9db4fe91521ca9fbf100b00b927a382fc22fdd62
haveagood@naver.com <haveagood@naver.com>
2020-04-06 11:36:42 +0900

    [BE] feat : 백엔드 개발 디렉토리 생성
    - 백엔드 개발 디렉토리를 일단 생성하였습니다.
    
    -------------------------
    -------------------------

---
+++ BE/src/main/java/todo3/codesquad/CodesquadApplication.java
@@ -0,0 +1,13 @@
+package todo3.codesquad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodesquadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodesquadApplication.class, args);
+    }
+
+}
---
+++ BE/src/test/java/todo3/codesquad/CodesquadApplicationTests.java
@@ -0,0 +1,13 @@
+package todo3.codesquad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodesquadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
