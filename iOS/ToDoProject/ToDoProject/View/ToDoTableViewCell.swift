9a6fc5651d532f63f14ddec41859da156b045a3b
dev-Lena <keunnalee@gmail.com>
2020-04-07 12:23:52 +0900

    [#4] Feat: TableView 관련 클래스 생성
    - TableView
    - TableViewCell
    - TableViewDataSource

---
+++ iOS/ToDoProject/ToDoProject/View/ToDoTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  ToDoTableViewCell.swift
+//  ToDoProject
+//
+//  Created by Keunna Lee on 2020/04/06.
+//  Copyright © 2020 dev-Lena. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewCell: UITableViewCell {
+    
+    static var identifier = "todoCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
