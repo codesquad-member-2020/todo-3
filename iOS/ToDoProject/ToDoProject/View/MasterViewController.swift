febbc011a6f90992841ed56f14240a8344a67fee
dev-Lena <keunnalee@gmail.com>
2020-04-07 12:21:54 +0900

    [#4] Feat: ViewController 클래스들 생성
    - To Do List 전체 화면 ViewController
    - 각 Column을 표시하는 Container View의 ViewController

---
+++ iOS/ToDoProject/ToDoProject/View/MasterViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.swift
+//  ToDoProject
+//
+//  Created by Keunna Lee on 2020/04/06.
+//  Copyright © 2020 dev-Lena. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
