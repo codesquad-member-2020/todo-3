20167cda5bfe25eadb58d200743db8bef1208df1
haveagood@naver.com <haveagood@naver.com>
2020-04-08 14:24:02 +0900

    [#18] Feat : 스키마 파일 작성
    - 데이터베이스에 Col, Card데이터를 저장할 테이블 생성
    - Col은 default값이 3가지 상태가 들어가므로 초기에 주입
    
    -------------------------
    -------------------------

---
+++ BE/src/main/resources/schema.sql
@@ -0,0 +1,27 @@
+drop table if exists col;
+drop table if exists card;
+
+CREATE TABLE col
+(
+    id       int primary key auto_increment,
+    col_name VARCHAR(100) not null,
+    deleted  boolean
+);
+
+
+
+CREATE TABLE card
+(
+    id           int primary key auto_increment,
+    title        varchar(100),
+    contents     varchar(500) not null,
+    writer       varchar(50)  not null,
+    deleted      boolean default false,
+    written_time datetime     not null,
+    col          int references col (id),
+    col_key      int
+);
+
+INSERT INTO col (col_name, deleted) VALUES ("To Do", false);
+INSERT INTO col (col_name, deleted) VALUES ("In Progress", false);
+INSERT INTO col (col_name, deleted) VALUES ("Done", false);
