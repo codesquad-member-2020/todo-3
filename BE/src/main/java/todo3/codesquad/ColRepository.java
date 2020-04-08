9f2fdef72b65b7aa7e9dc34328c13e1b93091562
sedin2 <53131108+sedin2@users.noreply.github.com>
2020-04-08 14:36:19 +0900

    [#17] Feat : findByColName 메소드 추가
    - ColName으로 Col을 찾아 반환 하는 메소드 추가

--- BE/src/main/java/todo3/codesquad/ColRepository.java
+++ BE/src/main/java/todo3/codesquad/ColRepository.java
@@ -1,7 +1,11 @@
 package todo3.codesquad;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface ColRepository extends CrudRepository<Col, Long> {
-    
+
+    @Query("SELECT * FROM col WHERE col_name = :colName")
+    Col findByColName(@Param("colName") String colName);
 }
