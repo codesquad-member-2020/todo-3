15d5b879786cea1ff65714ef5dcdccf031431ea6
dev-Lena <keunnalee@gmail.com>
2020-04-09 18:16:12 +0900

    [#31] Feat: 전체 ViewController에 각 Container View의 ViewController 추가

--- iOS/ToDoProject/ToDoProject/View/MasterViewController.swift
+++ iOS/ToDoProject/ToDoProject/View/MasterViewController.swift
@@ -10,8 +10,39 @@ import UIKit
 
 class MasterViewController: UIViewController {
 
+    @IBOutlet weak var toDoListContainerView: UIView!
+    @IBOutlet weak var inProgressListContainerView: UIView!
+    @IBOutlet weak var doneListContainerView: UIView!
+    
+    private var toDoListVC: ToDoManagerViewController!
+    private var inProgressListVC: ToDoManagerViewController!
+    private var doneListVC: ToDoManagerViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupChildViewControllers()
+    }
+    
+    private func setupChildViewControllers() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        let toDoListVC = storyboard.instantiateViewController(withIdentifier: "toDoListVC") as! ToDoManagerViewController
+        addChild(toDoListVC)
+        view.addSubview(toDoListContainerView)
+        self.toDoListVC = toDoListVC
+        
+        let inProgressListVC = storyboard.instantiateViewController(withIdentifier: "inProgressListVC") as! ToDoManagerViewController
+        addChild(inProgressListVC)
+        view.addSubview(inProgressListContainerView)
+        self.inProgressListVC = inProgressListVC
+
+        let doneListVC = storyboard.instantiateViewController(withIdentifier: "doneListVC") as! ToDoManagerViewController
+        addChild(doneListVC)
+        view.addSubview(doneListContainerView)
+        self.doneListVC = doneListVC
+    }
+    
+    private func setupHeader() {
     }
 }
 
