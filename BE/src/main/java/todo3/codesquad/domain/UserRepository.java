4d1212fe7e9378870bfc7ce2cf7ff1165bf900e9
haveagood@naver.com <haveagood@naver.com>
2020-04-12 17:24:24 +0900

    [#48] Feat : User name을 찾는 쿼리문 생성
    - 프론트에서 userId값을 받아오면, 해당 id값을 가지고 userName을 찾는 쿼리문을 작성하였습니다.
    
    # 꼬릿말은 아래에 작성 : ex) #이슈 번호
    
    # --COMMIT END--
    # <타입> 리스트
    # feat : 기능 (새로운 기능)
    # fix : 버그 (버그 수정)
    # refactor : 리팩토링
    # style : 스타일(코드 형식, 세미콜론 추가 : 비지니스 로직 변경 X)
    # docs : 문서 (문서 추가, 수정, 삭제)
    # test : 테스트 (테스트 코드 추가, 수정, 삭제 : 비지니스 로직 변경 X)
    # chroe : 기타 변경사항(빌드 스크립트 수정 등)
    -------------------------
    #	제목 첫 글자 대문자
    #	제목은 명령문
    #	제목 끝에 마침표(.) 금지
    #	제목과 본문을 한 줄 띄워 분리하기
    #	본문은 "어떻게" 보다 "무엇을" , "왜"를 설명한다
    #	본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    -------------------------

--- BE/src/main/java/todo3/codesquad/domain/UserRepository.java
+++ BE/src/main/java/todo3/codesquad/domain/UserRepository.java
@@ -1,4 +1,11 @@
 package todo3.codesquad.domain;
 
-public interface UserRepository {
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+
+public interface UserRepository extends CrudRepository<User,Long> {
+    @Query("SELECT user_name FROM user WHERE user_id = :userId")
+    String findByUserId(@Param("userId") String userId);
 }
