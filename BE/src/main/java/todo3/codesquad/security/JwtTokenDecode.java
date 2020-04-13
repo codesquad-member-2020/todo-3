0f8b613add21e8fdc6a62c9a6b1140d19e995e1d
haveagood@naver.com <haveagood@naver.com>
2020-04-13 17:41:21 +0900

    [#51] Feat : JWT 디코딩 메소드 구현
    - JWT를 받으면 디코딩하는 메소드를 구현하였습니다.
    
     - 시간초과 혹은 변조 에러가 뜨면 false를 리턴하고, 올바른 값이면 True를 리턴하도록 하였습니다.
    
    # --COMMIT END--
    # <타입> 리스트
    # feat : 기능 (새로운 기능)
    # fix : 버그 (버그 수정)
    # refactor : 리팩토링
    # style : 스타일(코드 형식, 세미콜론 추가 : 비지니스 로직 변경 X)
    # docs : 문서 (문서 추가, 수정, 삭제)
    # test : 테스트 (테스트 코드 추가, 수정, 삭제 : 비지니스 로직 변경 X)
    # chroe : 기타 변경사항(빌드 스크립트 수정 등)
    -------------------------
    #	제목 첫 글자 대문자
    #	제목은 명령문
    #	제목 끝에 마침표(.) 금지
    #	제목과 본문을 한 줄 띄워 분리하기
    #	본문은 "어떻게" 보다 "무엇을" , "왜"를 설명한다
    #	본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    -------------------------

--- BE/src/main/java/todo3/codesquad/security/JwtTokenDecode.java
+++ BE/src/main/java/todo3/codesquad/security/JwtTokenDecode.java
@@ -1,5 +1,23 @@
 package todo3.codesquad.security;
 
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import org.springframework.expression.ExpressionException;
+
+import javax.xml.bind.DatatypeConverter;
+
 public class JwtTokenDecode {
+    public boolean checkJwt(String jwt) {
+        try{
+            Claims claims = Jwts.parser().setSigningKey(DatatypeConverter.parseBase64Binary("todo"))
+                    .parseClaimsJws(jwt).getBody();
+            return true;
+        } catch (ExpressionException e){
+            return false;
+        } catch (JwtException e){
+            return false;
+        }
+    }
 }
