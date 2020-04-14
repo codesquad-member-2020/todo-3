b54509fa92b782fdc49328fc3a97262971f3b10d
sedin2 <53131108+sedin2@users.noreply.github.com>
2020-04-14 19:10:18 +0900

    [#57] Feature : JwtTokenDecode에 User 정보 가져오는 함수 생성
    - getLoginUser() 메소드 추가
    - Jwt 토큰을 사용하여 userName을 반환

--- BE/src/main/java/todo3/codesquad/security/JwtTokenDecode.java
+++ BE/src/main/java/todo3/codesquad/security/JwtTokenDecode.java
@@ -7,7 +7,10 @@ import io.jsonwebtoken.Jwts;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.expression.ExpressionException;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.xml.bind.DatatypeConverter;
 
 public class JwtTokenDecode {
@@ -16,17 +19,27 @@ public class JwtTokenDecode {
             LoggerFactory.getLogger(JwtTokenDecode.class);
 
     public boolean checkJwt(String jwt) {
-        try{
+        try {
             Claims claims = Jwts.parser().setSigningKey(DatatypeConverter.parseBase64Binary("todo"))
                     .parseClaimsJws(jwt).getBody();
             log.debug("{}", claims);
+            System.out.println(claims.get("userName"));
             return true;
-        } catch (ExpressionException e){
+        } catch (ExpressionException e) {
             log.debug("Token is timed out");
             return false;
-        } catch (JwtException e){
+        } catch (JwtException e) {
             log.debug("Token is not verified");
             return false;
         }
+    }
+
+    public String getLoginUser(String key) {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        String jwt = request.getHeader("Authorization");
+        Claims claims = Jwts.parser().setSigningKey(DatatypeConverter.parseBase64Binary("todo"))
+                .parseClaimsJws(jwt).getBody();
+        return (String) claims.get("userName");
+
     }
 }
