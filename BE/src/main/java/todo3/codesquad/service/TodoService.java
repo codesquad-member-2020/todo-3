31d24d90b8fd28b2647fee1cd9d59d8d087bb8b6
haveagood@naver.com <haveagood@naver.com>
2020-04-14 14:25:17 +0900

    [#47] refactor : Controller에서의 작업을 Service로 이동
    - 컨트롤러에서 처리하던 작업들을 Service로 이동시키고, Transcation 어노테이션을 설정
    
    # 꼬릿말은 아래에 작성 : ex) #이슈 번호
    
    # --COMMIT END--
    # <타입> 리스트
    # feat : 기능 (새로운 기능)
    # fix : 버그 (버그 수정)
    # refactor : 리팩토링
    # style : 스타일(코드 형식, 세미콜론 추가 : 비지니스 로직 변경 X)
    # docs : 문서 (문서 추가, 수정, 삭제)
    # test : 테스트 (테스트 코드 추가, 수정, 삭제 : 비지니스 로직 변경 X)
    # chroe : 기타 변경사항(빌드 스크립트 수정 등)
    -------------------------
    #	제목 첫 글자 대문자
    #	제목은 명령문
    #	제목 끝에 마침표(.) 금지
    #	제목과 본문을 한 줄 띄워 분리하기
    #	본문은 "어떻게" 보다 "무엇을" , "왜"를 설명한다
    #	본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    -------------------------

---
+++ BE/src/main/java/todo3/codesquad/service/TodoService.java
@@ -0,0 +1,58 @@
+package todo3.codesquad.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
+import todo3.codesquad.domain.Card;
+import todo3.codesquad.domain.Col;
+import todo3.codesquad.domain.ColRepository;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Service
+public class TempClass {
+
+    private final ColRepository colRepository;
+
+    public TempClass(ColRepository colRepository) {
+        this.colRepository = colRepository;
+    }
+
+    @Transactional
+    public Card createCard(Map<String, Object> cardData) {
+        String categoryName = cardData.get("categoryName").toString();
+        Col col = colRepository.findByCategoryName(categoryName).orElse(null);
+        List<Card> cards = col.getCards();
+        Card card = new Card(cardData);
+        cards.add(0, card);
+        for (int i = 0; i < cards.size(); i++) {
+            Card tempCard = cards.get(i);
+            tempCard.setRow(i + 1);
+        }
+        colRepository.save(col);
+        return card;
+    }
+
+    @Transactional
+    public Card updateCard(Long updateCardId, @RequestBody Map<String, Object> map) {
+        String colName = map.get("colName").toString();
+        Card updateCard = new Card();
+        Col col = colRepository.findByColName(colName).orElse(null);
+        if (col == null) {
+            return null;
+        }
+        List<Card> cards = col.getCards();
+        for (int i = 0; i < cards.size() ; i++) {
+            Long cardId = cards.get(i).getId();
+            if (cardId.equals(updateCardId)){
+                updateCard = cards.get(i);
+                updateCard.update(map);
+            }
+        }
+        colRepository.save(col);
+        return updateCard;
+    }
+}
