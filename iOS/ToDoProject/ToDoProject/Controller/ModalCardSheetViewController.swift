cada3623e50daf17bea93159fa014d01f3fa4f16
dev-Lena <keunnalee@gmail.com>
2020-04-16 13:33:15 +0900

    [#62] Refactor: Card Sheet Modal ViewController 수정

---
+++ iOS/ToDoProject/ToDoProject/Controller/ModalCardSheetViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ModalCardSheetViewController.swift
+//  ToDoProject
+//
+//  Created by Keunna Lee on 2020/04/16.
+//  Copyright © 2020 dev-Lena. All rights reserved.
+//
+
+import UIKit
+
+class ModalCardSheetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
