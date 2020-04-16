0c73aa4eb45a9102ec0a6d68633be7b366f510c0
kiyoesjh <hyeyaa125@gmail.com>
2020-04-17 07:04:56 +0900

    [#42] card 기능 구현

--- FE/src/view/Card.js
+++ FE/src/view/Card.js
@@ -2,35 +2,57 @@ import { getElement } from "../util/dom";
 import { fetchPOSTJSON, fetchDELETEJSON } from "../data/http";
 import { URL } from "../util/constant";
 import { modal } from "./popup";
+import { dragEventHandle } from "./dragDropEvent";
+import { controller } from "../controller/controller";
 
 export class Card {
-  constructor(parentColumnEle, data) {
-    Object.assign(this, data);
-    this.parentColumnEle = parentColumnEle;
-    this.removeCardHandle();
+  constructor({columnEle, columnCategoryName, columnId, cardData}) {
+    Object.assign(this, cardData);
+    this.parentColumnEle = columnEle;
+    this.columnCategoryName = columnCategoryName;
+    this.columnId = columnId;
+    this.index = this.row - 1;
+    // this.cardListener.call(this);
+    this.cardListener();
   }
 
-  removeCardHandle() {
-    this.parentColumnEle.addEventListener('click', this.removeCard);
+  cardListener() {
+    this.parentColumnEle.addEventListener('click', this.removeCardHandler.bind(this));
+    this.parentColumnEle.addEventListener('dblclick', this.editCardHandler);
   }
 
-  async removeCard() {
-    if(event.target.className !== 'delete-btn') return;
-    const card = event.target.closest('.card');
-    // const removeData = await fetchDELETEJSON(`${URL.CARD}/${card.dataset.id}`);
-    const alertHTML = modal.alert('삭제에 실패했습니다.');
-    getElement('.container').insertAdjacentHTML('beforeend', alertHTML);
-    // if(removeData.responseMessage !== 'Delete data is Success') return modal.alert('삭제에 실패했습니다.');
-    // card.remove();
+  removeCardHandler(event) {
+    if(event.target.className === 'delete-btn' && event.target.closest('.card').dataset.index === `${this.row}`) {
+      console.log(event.target)
+      const card = event.target.closest('.card');
+      const column = event.target.closest('.column');
+      if(!getElement('.container').querySelectorAll('.modal').length) modal.confirm('삭제하시겠습니까?');
+      console.log(this)
+      this.removeClickHandler.call(this, card, column);
+    }
   }
 
-  eventHandleWrap() {
-    
+  removeClickHandler(card, column) {
+    getElement('.modal .yes-btn').addEventListener('click', async () => {
+      const removeData = await fetchDELETEJSON(`${URL.DEFAULT}/columns/${column.dataset.id}/cards/${card.dataset.id}`);
+      modal.remove();
+      if(removeData.responseMessage !== 'Delete data is Success') return modal.alert('삭제에 실패했습니다.');
+      card.remove();
+      console.log(`${this.columnCategoryName}Cards`);
+      controller.responseData[`${this.columnCategoryName}Cards`].splice(this.index, 1);
+      column.querySelector('.card-length').textContent = controller.responseData[`${this.columnCategoryName}Cards`].length;
+    });
+  }
+
+  editCardHandler() {
+    // debugger
+    if(event.target.className !== 'comment') return;
+    modal.edit(event.target, this.columnId);
   }
 
   render() {
     return `
-      <div class="card" data-id="${this.id}" data-index="${this.row}">
+      <div class="card" id="card${this.id}" data-id="${this.id}" data-index="${this.row}">
         <p class="comment">${this.contents}</p>
         <span class="user-name">Add by <a href="#none">${this.writer}</a></span>
         <button type="button" class="delete-btn"><span class="blind">삭제</span></button>
--- FE/src/view/dragDropEvent.js
+++ FE/src/view/dragDropEvent.js
@@ -1,27 +1,85 @@
-import { getElement, getElements, hasClass } from '../util/dom.js';
+export const dragdropEvent = () => (function () {
+  const cards = document.querySelectorAll('.card');
+  let currentDroppable = null;
 
-const columnEle = getElement('.column');
-let isMove = false;
-let startPointX;
-let startPointY;
-const cardGap = 10;
-let cloneCardtNode;
-
-export const dragDropHandle = () => {
-  columnEle.addEventListener('mousedown', (e) => {
-    if(!hasClass(e.target, 'card')) return;
-    const target = e.target;
-    cloneCardtNode = target.cloneNode(true);
-    console.log(cloneCardtNode);
-    isMove = true;
-    startPointX = e.offsetX;
-    startPointY = e.offsetY;
+  cards.forEach((card) => {
+    card.addEventListener('mousedown', mouseDownHandler);
   })
 
-  columnEle.addEventListener('mousemove', (e) => {
-    if(!hasClass(e.target, 'card')) return;
-    if(!isMove) return;
-    const target = e.currentTarget;
-    
+  function mouseDownHandler () {
+    if(event.target.tagName === 'P' || event.target.tagName === 'BUTTON' || event.target.tagName === 'A' ) return;
+    const cardBefore = event.currentTarget;
+    const card = event.currentTarget.cloneNode(true);
+    cardBefore.closest('.column').insertBefore(card, cardBefore);
+    let shiftX = event.clientX - card.getBoundingClientRect().left;
+    let shiftY = event.clientY - card.getBoundingClientRect().top;
+    let pageX = event.pageX;
+    let pageY = event.pageY;
+    cardBefore.classList.add('drag-target');
+    const cardWidth = cardBefore.offsetWidth;
+    card.classList.add('dragging');
+    card.style.width = cardWidth + 'px';
+    document.querySelector('.container').append(card);
+
+    moveAt({card, pageX, pageY, shiftX, shiftY});
+    const bindMouseMove = onMouseMove.bind(null, card, shiftX, shiftY);
+    document.addEventListener('mousemove', bindMouseMove);
+    card.addEventListener('mouseup', () => {
+      document.removeEventListener('mousemove', bindMouseMove);
+      if(!currentDroppable) return;
+      card.remove();
+      currentDroppable.appendChild(cardBefore);
+      cardBefore.classList.remove('drag-target');
+      currentDroppable.style.background = '';
+    })
+  }
+
+  function moveAt({card, pageX, pageY, shiftX, shiftY}) {
+    card.style.left = pageX - shiftX + 'px';
+    card.style.top = pageY - shiftY + 'px';
+  }
+
+  function onMouseMove(card, shiftX, shiftY) {
+    const pageX = event.pageX;
+    const pageY = event.pageY;
+    moveAt({card, pageX, pageY, shiftX, shiftY});
+
+    card.hidden = true;
+    let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+    card.hidden = false;
+
+    if (!elemBelow) return;
+
+    let droppableBelow = elemBelow.closest('.column');
+    if (currentDroppable != droppableBelow) {
+      if (currentDroppable) {
+        leaveDroppable(currentDroppable);
+      }
+      currentDroppable = droppableBelow;
+      if (currentDroppable) {
+        enterDroppable(currentDroppable);
+      }
+    }
+    if(!currentDroppable) return;
+    const currentColumnCard = currentDroppable.querySelectorAll('.card');
+    currentColumnCard.forEach((card, idx) => {
+      // console.log(card.offsetTop);
+      // const cardHeight = card.offsetHeight / 2;
+      // if()
+    })
+  }
+
+  function enterDroppable(elem) {
+    elem.style.background = 'pink';
+  }
+
+  function leaveDroppable(elem) {
+    elem.style.background = '';
+  }
+
+  cards.forEach((card) => {
+    card.addEventListener('dragstart', () => {
+      return false
+    });
   })
-}
+})();
