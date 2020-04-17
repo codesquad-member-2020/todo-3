c8820aefaca06a70d5a17c69113118b97bc71b92
dev-Lena <keunnalee@gmail.com>
2020-04-17 17:50:08 +0900

    [#71] Feat: Pop over Menu 기능 추가
    - 각 컬럼의 카드를 모두 삭제할 수 있는 메뉴 추가

--- iOS/ToDoProject/ToDoProject/Controller/PoPOverMenuViewController.swift
+++ iOS/ToDoProject/ToDoProject/Controller/PoPOverMenuViewController.swift
@@ -8,23 +8,57 @@
 
 import UIKit
 
-class PoPOverMenuViewController: UIViewController {
-
-    override func viewDidLoad() {
+class PoPOverMenuViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
+{
+    var arrayListPopover : [String] = []
+    private let tableView = UITableView()
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.isScrollEnabled = true
+        tableView.backgroundColor = UIColor.clear
+        tableView.bounces = false
+        configureSizeForChildView(forTableView: 300)
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
     }
-    */
-
+    
+    func configureSizeForChildView(forTableView width : CGFloat)
+    {
+        var tempSize = self.presentingViewController?.view.bounds.size
+        tempSize?.width = width
+        let size = tableView.sizeThatFits(tempSize!)
+        self.preferredContentSize = size
+        self.view.systemLayoutSizeFitting(tableView.frame.size)
+        self.tableView.frame = self.view.frame
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return arrayListPopover.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cellIdentifer: String = "cell"
+        var cell: UITableViewCell? = tableView.dequeueReusableCell(withIdentifier: cellIdentifer)
+        if cell == nil{
+            cell = UITableViewCell(style: .default, reuseIdentifier: cellIdentifer)
+        }
+        cell?.backgroundColor = UIColor.clear
+        cell?.textLabel?.text = "\(arrayListPopover[indexPath.row])"
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        NotificationCenter.default.post(name:  NSNotification.Name(rawValue: "click"), object: indexPath)
+    }
+    
 }
