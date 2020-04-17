48cdf14f3248464df6802a084e8ceebdb8cb61bd
dev-Lena <keunnalee@gmail.com>
2020-04-17 17:57:18 +0900

    [# 62] Feat: TableViewDelegate 기능 구현
    - Context Menu 표시 및 메뉴별 메소드 연결
    - Drag and Drop 관련 메소드 추가

--- iOS/ToDoProject/ToDoProject/Controller/TableViewDelegate.swift
+++ iOS/ToDoProject/ToDoProject/Controller/TableViewDelegate.swift
@@ -14,22 +14,49 @@ extension ToDoManagerViewController: UITableViewDelegate{
         var configuration = UIContextMenuConfiguration()
         if self.column == ColumnURLName.done{
             configuration = UIContextMenuConfiguration(identifier: nil, previewProvider: nil) { _ in
-                let edit = UIAction(title: "edit...") { _ in
+                let moveToToDo = UIAction(title: "move to To Do", image: UIImage(systemName: SystemImageName.arrowToLeftTwice)) { _ in
+                    let toDoColumnId = 1
+                    self.moveCard(indexPath: indexPath, tableView: tableView, columnId: toDoColumnId) }
+                let moveToInProgress = UIAction(title: "move to In Progress", image: UIImage(systemName: SystemImageName.arrowToLeft)) { _ in
+                    let doneColumnId = 2
+                    self.moveCard(indexPath: indexPath, tableView: tableView, columnId: doneColumnId) }
+
+                let edit = UIAction(title: "edit", image: UIImage(systemName: SystemImageName.pencil)) { _ in
                     self.editCard(indexPath: indexPath, tableView: tableView) }
-                let delete = UIAction(title: "delete", attributes: .destructive) { _ in
+                let delete = UIAction(title: "delete", image: UIImage(systemName: SystemImageName.trashBin), attributes: .destructive) { _ in
                     self.deleteCard(indexPath: indexPath, tableView: tableView) }
-                let menu = UIMenu(title: "", children: [edit, delete])
+                let menu = UIMenu(title: "", children: [moveToToDo, moveToInProgress, edit, delete])
+                return menu
+            }            
+        } else if self.column == ColumnURLName.inProgress{
+            configuration = UIContextMenuConfiguration(identifier: nil, previewProvider: nil) { _ in
+                let moveToToDo = UIAction(title: "move to To Do", image: UIImage(systemName: SystemImageName.arrowToLeft)) { _ in
+                    let toDoColumnId = 1
+                    self.moveCard(indexPath: indexPath, tableView: tableView, columnId: toDoColumnId) }
+                let moveToDone = UIAction(title: "move to Done", image: UIImage(systemName: SystemImageName.arrowToRight)) { _ in
+                    let doneColumnId = 3
+                    self.moveCard(indexPath: indexPath, tableView: tableView, columnId: doneColumnId) }
+                let edit = UIAction(title: "edit", image: UIImage(systemName: SystemImageName.pencil)) { _ in
+                    self.editCard(indexPath: indexPath, tableView: tableView) }
+                let delete = UIAction(title: "delete", image: UIImage(systemName: SystemImageName.trashBin), attributes: .destructive) { _ in
+                    self.deleteCard(indexPath: indexPath, tableView: tableView) }
+                let menu = UIMenu(title: "", children: [moveToToDo, moveToDone, edit, delete])
                 return menu
             }
+
         } else {
             configuration = UIContextMenuConfiguration(identifier: nil, previewProvider: nil) { _ in
-                let moveToDone = UIAction(title: "move to done") { _ in
-                    self.moveCardToDone(indexPath: indexPath, tableView: tableView) }
-                let edit = UIAction(title: "edit...") { _ in
+                let moveToInProgress = UIAction(title: "move to In Progress", image: UIImage(systemName: SystemImageName.arrowToRight)) { _ in
+                    let doneColumnId = 2
+                    self.moveCard(indexPath: indexPath, tableView: tableView, columnId: doneColumnId) }
+                let moveToDone = UIAction(title: "move to Done", image: UIImage(systemName: SystemImageName.arrowToRight)) { _ in
+                    let doneColumnId = 3
+                    self.moveCard(indexPath: indexPath, tableView: tableView, columnId: doneColumnId) }
+                let edit = UIAction(title: "edit", image: UIImage(systemName: SystemImageName.pencil)) { _ in
                     self.editCard(indexPath: indexPath, tableView: tableView) }
-                let delete = UIAction(title: "delete", attributes: .destructive) { _ in
+                let delete = UIAction(title: "delete", image: UIImage(systemName: SystemImageName.trashBin), attributes: .destructive) { _ in
                     self.deleteCard(indexPath: indexPath, tableView: tableView) }
-                let menu = UIMenu(title: "", children: [moveToDone, edit, delete])
+                let menu = UIMenu(title: "", children: [moveToInProgress, moveToDone, edit, delete])
                 return menu
             }
 
@@ -38,25 +65,22 @@ extension ToDoManagerViewController: UITableViewDelegate{
     }
     
     private func deleteCard(indexPath: IndexPath, tableView: UITableView){
-        let cardToDelete = self.dataManager.cardsData?.responseData.cards[indexPath.row]
-        guard let cardIdToDelete = cardToDelete?.id else { return }
-        self.dataManager.cardsData?.responseData.cards.remove(at: indexPath.row)
+        let cardToDelete = self.dataSource.dataManager.cardsData?.responseData.cards[indexPath.row]
+        guard let cardId = cardToDelete?.id else { return }
+        let columnId = self.dataManager.switchURLColumnId(column: self.column)
+        self.dataSource.dataManager.cardsData?.responseData.cards.remove(at: indexPath.row)
         tableView.deleteRows(at: [indexPath], with: .fade)
-        guard let columnId = self.dataManager.cardsData?.responseData.id else { return }
-        guard let cardId = self.dataManager.cardsData?.responseData.cards[indexPath.row].id else { return }
-        self.dataManager.requestDeleteCard(columnId: columnId, cardId: cardId)
+        self.dataSource.dataManager.requestDeleteCard(columnId: columnId, cardId: cardId)
     }
     
-    private func moveCardToDone(indexPath: IndexPath, tableView: UITableView){
+    private func moveCard(indexPath: IndexPath, tableView: UITableView, columnId: Int){
         let currentColumn = self.switchName(column: self.column)
-        let doneColumnId = 3
-        let toLast = 0
-        let moveCard = MoveCardForm(destinationId: doneColumnId, destinationRow: 9)
+        let toLast = 99 // 백엔드와 상의해서 99로 보내면 Destination Column에 마지막에 붙음
+        let moveCard = MoveCardForm(destinationId: columnId, destinationRow: toLast)
         guard let columnId = self.dataManager.cardsData?.responseData.id else { return }
         guard let cardId = self.dataManager.cardsData?.responseData.cards[indexPath.row].id else { return }
         self.dataManager.requestMoveCard(movingInfo: moveCard, originalColumnId: columnId, originalCardId: cardId, originalCardRow: indexPath.row)
     }
-    
     
     private func editCard(indexPath: IndexPath, tableView: UITableView){
         let cardSheet = ModalCardSheetViewController()
@@ -74,4 +98,14 @@ extension ToDoManagerViewController: UITableViewDelegate{
         cardSheet.contentsTextField.text = originalCard?.contents
         self.present(cardSheet, animated: true, completion: nil)
     }
+    
+    // Drag & Drop
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        self.dataSource.moveItem(at: sourceIndexPath.row, to: destinationIndexPath.row)
+    }
+
 }
